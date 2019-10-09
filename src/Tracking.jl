09cd6d2a7d4bf140b7789ecccdd80690d6cf0303
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-09 16:17:19 +0000

    Snapshot of branch master taken on 2019-10-09T12:17:18.887-04:00 from "https://github.com/UnofficialJuliaMirror/Tracking.jl-10b2438b-ffd4-5096-aa58-44041d5c8f3b"

--- src/Tracking.jl
+++ src/Tracking.jl
@@ -83,30 +83,20 @@ module Tracking
     """
     $(SIGNATURES)
 
-    Simplified initials in the case that only the carrier doppler and the code
-    phase is available.
-    """
-    function TrackingInitials(carrier_doppler, code_phase)
-        TrackingInitials(carrier_doppler, 0.0, 0.0Hz, code_phase)
-    end
-
-    """
-    $(SIGNATURES)
-
     Initials with estimated code doppler from the carrier doppler.
     """
-    function TrackingInitials(system::AbstractGNSSSystem, carrier_doppler, code_phase)
-        TrackingInitials(carrier_doppler, 0.0, carrier_doppler * (system.code_freq / system.center_freq), code_phase)
+    function TrackingInitials(::Type{T}, carrier_doppler, code_phase) where T <: AbstractGNSSSystem
+        TrackingInitials(carrier_doppler, 0.0, carrier_doppler * get_code_center_frequency_ratio(T), code_phase)
     end
 
-    function CodeShift(system::S, sample_freq, preferred_code_shift) where S <: Union{GPSL1, GPSL5}
-        sample_shift, actual_code_shift = calc_shifts(system, sample_freq, preferred_code_shift)
+    function CodeShift(::Type{T}, sample_freq, preferred_code_shift) where T <: AbstractGNSSSystem
+        sample_shift, actual_code_shift = calc_shifts(T, sample_freq, preferred_code_shift)
         CodeShift{3}(sample_shift, actual_code_shift)
     end
 
-    function calc_shifts(system::AbstractGNSSSystem, sample_freq, preferred_code_shift)
-        sample_shift = round(preferred_code_shift * sample_freq / system.code_freq)
-        actual_code_shift = sample_shift * system.code_freq / sample_freq
+    function calc_shifts(::Type{T}, sample_freq, preferred_code_shift) where T <: AbstractGNSSSystem
+        sample_shift = round(preferred_code_shift * sample_freq / get_code_frequency(T))
+        actual_code_shift = sample_shift * get_code_frequency(T) / sample_freq
         sample_shift, actual_code_shift
     end
 
@@ -129,5 +119,6 @@ module Tracking
     include("tracking_loop.jl")
     include("gpsl1.jl")
     include("gpsl5.jl")
+    include("galileo_e1b.jl")
     include("phased_array.jl")
 end
